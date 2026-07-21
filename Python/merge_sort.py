8f2c4e0377694a9ef27c06d2998411aa0ed8c0d1
Croopzy <61269360+Croopzy@users.noreply.github.com>
2026-07-21 23:19:27 +0100

    UPDATE merge_sort.py

--- Python/merge_sort.py
+++ Python/merge_sort.py
@@ -17,17 +17,19 @@ def merge(arrIn, low, mid, high, reverse = False):
       arrIn[posM] = arrIn[pos2] # Write Current Element From Second Array To Merged Array
       pos2 += 1  # Next Item in Second Half
     posM += 1  # Always Increment Position in Merged Array
+  # endwhile
 
   while pos1 < len1:  # Executes IF pos2 == (high + 1)
     arrIn[posM] = arr1[pos1]  # Copy Remaining Buffer Array To Merged Array
     pos1 += 1  # Next Item in Buffer Array
     posM += 1  # Increment Position in Merged Array
+  # endwhile
 
   while pos2 <= high:  # Executes IF pos1 == len1
     arrIn[posM] = arrIn[pos2]  # Copy Remaining Second Half To Merged Array
     pos2 += 1  # Next Item in Second Half
     posM += 1  # Increment Position in Merged Array
-#endprocedure
+# endprocedure
 
 
 # Full Merge Sort Algorithm
@@ -37,5 +39,5 @@ def merge_sort(arrIn, low, high, reverse = False):
     merge_sort(arrIn, low, mid, reverse) # Sort First Half
     merge_sort(arrIn, (mid + 1), high, reverse) # Sort Second Half
     merge(arrIn, low, mid, high, reverse) # Merge Sorted Halves
-#endprocedure
+# endprocedure
     
